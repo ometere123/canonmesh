a4e36c72628ec5b100a05e94ded480a010da9905
papitodele <45469370+ometere123@users.noreply.github.com>
2026-08-26 01:10:28 +0100

    feat: add branch genealogy route

---
+++ app/worlds/[worldId]/branches/page.tsx
@@ -0,0 +1,2 @@
+import {BranchMap} from "@/components/views";
+export default async function Page({params}:{params:Promise<{worldId:string}>}){const{worldId}=await params;return <BranchMap worldId={Number(worldId)}/>}
